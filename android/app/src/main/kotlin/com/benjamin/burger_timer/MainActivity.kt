8460cd95d530485d59cd56ec2adbf5ab9451925f
benjamin32561 <ben32561@gmail.com>
2021-03-15 15:48:35 +0200

    fixed name

---
+++ android/app/src/main/kotlin/com/benjamin/burger_timer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.benjamin.burger_timer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:willis/main.dart';
+import 'package:burger_timer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
